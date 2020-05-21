959ac3bb135be7bd165f6783c1c02af95cdbd8cc
melevir <melevir@gmail.com>
2020-05-21 09:02:34 +0300

    refactor airtable api

--- learn_python_bot/api/airtable.py
+++ learn_python_bot/api/airtable.py
@@ -1,5 +1,5 @@
 import os
-from typing import List, Any, Mapping, NamedTuple
+from typing import List, Any, Mapping, NamedTuple, Optional
 
 from requests import get, patch, post
 
@@ -35,37 +35,42 @@ class AirtableAPI(NamedTuple):
         )
 
     def fetch_students_data_from_airtable(self) -> List[Mapping[str, Any]]:
-        airtable_response = get(
-            f'https://api.airtable.com/v0/{self.airtable_base_id}/current_course',
+        raw_airtable_data = self._make_airtable_request(
+            'current_course',
             params={'view': self.students_list_view_name},
-            headers={'Authorization': f'Bearer {self.airtable_api_token}'},
         )
-        raw_airtable_data = airtable_response.json() if airtable_response else None
-        return raw_airtable_data.get('records', [])
+        return raw_airtable_data.get('records', []) if raw_airtable_data else []
 
     def extract_students(self, raw_students_data: List[Mapping[str, Any]]) -> List[Student]:
         return [self._extract_student_record(r) for r in raw_students_data]
 
-    def set_telegram_chat_id(self, student_airtable_id: str, telegram_chat_id: int) -> Student:
-        response = patch(
-            f'https://api.airtable.com/v0/{self.airtable_base_id}/current_course',
-            headers={'Authorization': f'Bearer {self.airtable_api_token}'},
-            json={'records': [{'id': student_airtable_id, 'fields': {'chat_id': str(telegram_chat_id)}}]},
-        ).json()
-        return self._extract_student_record(response['records'][0])
+    def set_telegram_chat_id(self, student_airtable_id: str, telegram_chat_id: int) -> Optional[Student]:
+        raw_airtable_data = self._make_airtable_request(
+            'current_course',
+            method='patch',
+            json={'records': [{
+                'id': student_airtable_id,
+                'fields': {'chat_id': str(telegram_chat_id)},
+            }]},
+        )
+        return (
+            self._extract_student_record(raw_airtable_data['records'][0])
+            if raw_airtable_data
+            else None
+        )
 
     def student_has_feedback_for_week(self, week_num, student_airtable_pk) -> bool:
-        airtable_response = get(
-            f'https://api.airtable.com/v0/{self.airtable_base_id}/weekly_feedback',
+        raw_airtable_data = self._make_airtable_request(
+            'weekly_feedback',
             params={'filterByFormula': f'AND(student={student_airtable_pk},week_num={week_num})'},
-            headers={'Authorization': f'Bearer {self.airtable_api_token}'},
-        ).json()
-        return bool(airtable_response['records'])
+        )
+
+        return bool(raw_airtable_data['records']) if raw_airtable_data else False
 
     def save_feedback(self, student_airtable_id, week_num, is_liked) -> None:
-        post(
-            f'https://api.airtable.com/v0/{self.airtable_base_id}/weekly_feedback',
-            headers={'Authorization': f'Bearer {self.airtable_api_token}'},
+        self._make_airtable_request(
+            'weekly_feedback',
+            method='post',
             json={'records': [
                 {'fields': {
                     'student': [student_airtable_id],
@@ -73,12 +78,28 @@ class AirtableAPI(NamedTuple):
                     'liked': is_liked,
                 }},
             ]},
-        ).json()
+        )
 
     def fetch_feedback_for_week(self, course_week_num: int) -> List[Mapping[str, Any]]:
-        airtable_response = get(
-            f'https://api.airtable.com/v0/{self.airtable_base_id}/weekly_feedback',
+        raw_airtable_data = self._make_airtable_request(
+            'weekly_feedback',
             params={'filterByFormula': f'week_num={course_week_num}'},
+        )
+
+        return raw_airtable_data['records'] if raw_airtable_data else []
+
+    def _make_airtable_request(
+        self,
+        table_name,
+        method='get',
+        params=None,
+        json=None,
+    ) -> Optional[Mapping[str, Any]]:
+        methods_map = {f.__name__: f for f in [get, post, patch]}
+        airtable_response = methods_map[method](  # type: ignore
+            f'https://api.airtable.com/v0/{self.airtable_base_id}/{table_name}',
+            params=params or {},
             headers={'Authorization': f'Bearer {self.airtable_api_token}'},
-        ).json()
-        return airtable_response['records']
+            json=json or None,
+        )
+        return airtable_response.json() if airtable_response else None
